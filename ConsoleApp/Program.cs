6767eb01921f5ed3101a4456bf6cd8e1295a7776
Joaquin Umpierrez <jo.umpierrez@gmail.com>
2018-05-17 14:21:06 -0300

    Terminado

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -5,6 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.Entity;
 using System.Data.Common;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.Core.Objects;
+using System.Data.SqlClient;
 
 namespace ConsoleApp
 {
@@ -19,6 +22,62 @@ namespace ConsoleApp
                 // Crear comando para ejecutar el Stored Procedure
                 DbCommand cmd = db.Database.Connection.CreateCommand();
                 cmd.CommandText = "[dbo].[GetAllPeople]";
+                try
+                {
+                    db.Database.Connection.Open(); // Abre la conexion
+
+                    // Utilizando el Stored Procedure para crear
+                    Console.WriteLine("-------> Crear Persona <-------");
+                    Console.WriteLine("Ingrese el Documento");
+                    string documentToAdd = Console.ReadLine();
+                    Console.WriteLine("Ingrese el Nombre");
+                    string nameToAdd = Console.ReadLine();
+                    SqlParameter docAdd = new SqlParameter("@doc", documentToAdd);
+                    SqlParameter namAdd = new SqlParameter("@name", nameToAdd);
+                    db.Database.ExecuteSqlCommand("NewPerson @doc, @name", docAdd, namAdd);
+                    
+
+
+                    // Utilizando el Stored Procedure para modificar
+                    Console.WriteLine("");
+                    Console.WriteLine("-------> Modificar Persona <-------");
+                    Console.WriteLine("Ingrese el Documento");
+                    string documentToUpdate = Console.ReadLine();
+                    Console.WriteLine("Ingrese el Nombre");
+                    string nameToUpdate = Console.ReadLine();
+                    SqlParameter docUpd = new SqlParameter("@doc", documentToUpdate);
+                    SqlParameter namUpd = new SqlParameter("@name", nameToUpdate);
+                    db.Database.ExecuteSqlCommand("UpdatePerson @doc, @name", docUpd, namUpd);
+
+
+                    // Utilizando el Stored Procedure para borrar
+                    Console.WriteLine("");
+                    Console.WriteLine("-------> Borrar Persona <-------");
+                    Console.WriteLine("Ingrese el Documento a borrar");
+                    string documentToDelete = Console.ReadLine();
+                    SqlParameter delP = new SqlParameter("@doc", documentToDelete);
+                    db.Database.ExecuteSqlCommand("DeletePerson @doc", delP);
+                    Console.WriteLine("");
+
+                    DbDataReader reader = cmd.ExecuteReader(); // Ejecuta el Stored Procedure
+                    // Leemos las Personas
+                    ObjectResult<Person> people = ((IObjectContextAdapter)db)
+                        .ObjectContext
+                        .Translate<Person>(reader, "people",
+                        MergeOption.AppendOnly);
+
+                    // Mostramos las personas
+                    foreach (Person item in people)
+                    {
+                        Console.WriteLine($"{item.Document} --- {item.name}");
+                    }
+                    reader.Close(); // Cerramos la ejecucion del Stored Procedure
+                }
+                finally
+                {
+                    db.Database.Connection.Close(); // Cerramos la conexion
+                }
+                Console.ReadKey();
             }
         }
     }
---
+++ QueryStoreProceudres(Insert, Update).sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[NewClient] (@doc nchar(15), @name nchar(50))
+AS BEGIN
+INSERT INTO People (Document, name) values (@doc, @name);
+END
+GO
+
+CREATE PROCEDURE [dbo].[UpdateClient] (@doc nchar(15), @name nchar(50))
+AS BEGIN
+UPDATE People SET name = @name WHERE Document = @doc;
+END
+GO
+
+EXEC sp_rename NewClient, NewPerson;
+EXEC sp_rename UpdateClient, UpdatePerson;
